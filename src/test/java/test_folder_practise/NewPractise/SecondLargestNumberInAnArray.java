7aa0f26731ad18af54b18d69728ec58604f5bb8e
nikhilkumarthota8-dev <Nikhilkumarthota8@gmail.com>
2026-06-08 20:54:59 +0530

    Adding Array Programs

--- src/test/java/test_folder_practise/NewPractise/SecondLargestNumberInAnArray.java
+++ src/test/java/test_folder_practise/NewPractise/SecondLargestNumberInAnArray.java
@@ -2,13 +2,13 @@ package test_folder_practise.NewPractise;
 
 public class SecondLargestNumberInAnArray {
     public static void main(String[] args) {
-        int[] numbers = {1, 2, 3, 4, 5, 6, 78, 88, 99, 101, 1002};
+        int[] numbers = {1, 2, 3, 4, 5000, 6, 78, 88, 99, 101, 1002};
         int maxNumber = numbers[0];
         int secondMaxNumber = numbers[0];
         for (int i = 0; i < numbers.length; i++) {
             if (numbers[i] > maxNumber) {
-                maxNumber = numbers[i];
                 secondMaxNumber = maxNumber;
+                maxNumber = numbers[i];
             } else if (secondMaxNumber < numbers[i] && maxNumber > secondMaxNumber) {
                 secondMaxNumber = numbers[i];
             }
